68e2e0bc582e0359a79c999b870da32209eb6a20
Maverik Minett <maverik@maverik.io>
2025-09-03 18:41:19 -0400

    split library

--- src/util/lib/util.ts
+++ src/util/lib/util.ts
@@ -1,4 +1,5 @@
-import { Class } from '../..'
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { Class } from '@agape/types';
 
 export function classExtends(target: Class, ancestor: Class) {
   if (target === ancestor) {
